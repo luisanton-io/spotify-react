7e8d9e1c6f2e795c2da5e9b1e870d0d084579123
Luis Antonio Canettoli <hello@luisanton.io>
2020-07-31 14:37:44 +0200

    Footer reading from Shared State

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { connect } from 'react-redux'
 import {faHeart as filledHeart} from '@fortawesome/free-solid-svg-icons'
 import {faHeart as emptyHeart} from '@fortawesome/free-regular-svg-icons'
 
-// const mapStateToProps = (state) => state;
+const mapStateToProps = (state) => state;
 
 const mapDispatchToProps = (dispatch) => ({
   toggleLike: (id, like) =>
@@ -19,33 +19,36 @@ const mapDispatchToProps = (dispatch) => ({
 const Footer = (props) => {
     console.log(props.albumCover)
     console.log(props)
-    let liked = props.likedSongs.some( id => id === props.playingQueue.nowPlaying.id )
+    let nowPlaying = props.playingQueue.nowPlaying
+    let liked = nowPlaying ? props.likedSongs.likedList.some( id => id === nowPlaying.id ) : null
     return (        
         <div className="container-fluid fixed-bottom bg-container pt-1">
             <div className="row">
                 <div className="col">
                     {
-                        props.location.pathname === '/album/'+ props.albumId && (
+                        props.location.pathname === '/album/' && (
                         <div className="row text-white">
                             <div className="col-4">
                                 <img
-                                    src={props.playingQueue.nowPlaying.albumCover}
+                                    src={nowPlaying.albumCover}
                                     style={{height:"5rem", width: "5rem"}}
                                     className="card-img img-fluid"
                                     alt="cover image of album"
                                 />
-                                
                             </div>
                             <div className="col d-flex py-3">
                                 <div className="d-flex flex-column justify-content-center">
-                                    <small>{props.playingQueue.nowPlaying.albumLabel}</small>
-                                    <small>{props.playingQueue.nowPlaying.albumTitle}</small>
+                                    <small>{nowPlaying.albumLabel}</small>
+                                    <small>{nowPlaying.albumTitle}</small>
                                 </div>
                                 <div className="d-flex flex-column justify-content-center">
-                                    <FontAwesomeIcon 
-                                        icon={ liked ? filledHeart : emptyHeart } 
-                                        onClick={ () => props.toggleLike(props.playingQueue.nowPlaying, !liked) } 
-                                        />
+                                    {
+                                        liked &&
+                                        <FontAwesomeIcon 
+                                            icon={ liked ? filledHeart : emptyHeart } 
+                                            onClick={ () => props.toggleLike(nowPlaying, !liked) } 
+                                            />
+                                    }
                                 </div>
                                     
                             </div>
@@ -97,5 +100,5 @@ const Footer = (props) => {
     )
 }
 
-// export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Footer))
-export default (withRouter(Footer))
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Footer))
+// export default (withRouter(Footer))
--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
 ReactDOM.render(
   //<React.StrictMode>
-    <App />,
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>,
   //</React.StrictMode>,
   document.getElementById('root')
 );
