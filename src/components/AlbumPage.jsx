ebde2604c4a4920de5d0b9998f75921a37a011a1
Luis Antonio Canettoli <hello@luisanton.io>
2020-07-31 15:02:41 +0200

    Update AlbumPage.jsx

--- src/components/AlbumPage.jsx
+++ src/components/AlbumPage.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import Actions from '../consts/Actions';
+
+
+const mapStateToProps = (state) => state;
+
+const mapDispatchToProps = (dispatch) => ({
+  setPlayingNow: (track) =>
+    dispatch({
+      type: Actions.playingQueue.set,
+      payload: track
+    }),
+});
 
 class Album extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       albums: [],
-      tracks:[],
-      artist:[],
+      tracks: [],
+      artist: [],
     }
-    console.log(this.props)
+    // console.log(this.props)
   }
-  
-  
+
+
   albumToFooter = (albumId, albumCover, albumLabel, albumTitle) => (
     this.props.sendAlbum(albumId, albumCover, albumLabel, albumTitle)
-    )
+  )
 
-  componentDidMount = () =>{
+  componentDidMount = () => {
     let albumId = this.props.match.params.id;
 
     let headers = new Headers({
@@ -41,77 +54,77 @@ class Album extends React.Component {
         let tracks = albumList.tracks.data
 
         this.albumToFooter(albumId, album.cover_small, album.label, album.title)
-        
-        this.setState({albums: album, artist: artist, tracks: tracks})
-        console.log(this.state.albums)
-        console.log(this.state.artist)
-        console.log(this.state.tracks)
+
+        this.setState({ albums: album, artist: artist, tracks: tracks })
+        // console.log(this.state.albums)
+        // console.log(this.state.artist)
+        // console.log(this.state.tracks)
 
       });
   }
 
 
-  
+
   albumArt = (album, artist) => {
     return (
       <>
-    <img
-      src={album.cover_medium}
-      className="card-img img-fluid"
-      alt="cover image of ${album.label}"
-    />
-    <div className="mt-4 text-center">
-      <p className="album-title">{album.label}</p>
-    </div>
-    <div className="text-center">
-      <Link to={`/artist/`+artist.id}><p className="artist-name">{artist.name}</p></Link>
-    </div>
-    <div className="mt-4 text-center">
-      <button id="btnPlay" className="btn btn-success" type="button">
-        Play
+        <img
+          src={album.cover_medium}
+          className="card-img img-fluid"
+          alt="cover image of ${album.label}"
+        />
+        <div className="mt-4 text-center">
+          <p className="album-title">{album.label}</p>
+        </div>
+        <div className="text-center">
+          <Link to={`/artist/` + artist.id}><p className="artist-name">{artist.name}</p></Link>
+        </div>
+        <div className="mt-4 text-center">
+          <button id="btnPlay" className="btn btn-success" type="button">
+            Play
       </button>
-    </div>
-    </>
+        </div>
+      </>
     );
   };
 
   song = (song) => {
-    return(
-    song.map((track, i)=>{
-      return (
-        <div className="py-3 trackHover" key={i}>
-          <a
-            href="#"
-            className="card-title trackHover px-3"
-            style={{color: "white"}}
-            // onClick={() => this.props.updateQueue(track)}
+    return (
+      song.map((track, i) => {
+        return (
+          <div className="py-3 trackHover" key={i} onClick={() => this.props.setPlayingNow(track)}>
+            <a
+              href="#"
+              className="card-title trackHover px-3"
+              style={{ color: "white" }}
+              onClick={() => this.props.setPlayingNow(track)}
             >{track.title}</a>
-          <small className="duration pr-3" style={{color: "white"}}
+            <small className="duration pr-3" style={{ color: "white" }}
             >{(track.duration / 60).toFixed(2)}</small>
-        </div>
+          </div>
         );
-    })
+      })
     )
   };
-          
 
 
-  render(){
+
+  render() {
     return (
-        <div className="col-12 col-md-9 offset-md-3 mainPage">
-          <div className="row">
-            <div className="col-md-3 pt-5 text-center" id="img-container">{this.albumArt(this.state.albums, this.state.artist)}</div>
-            <div className="col-md-8 p-5">
-              <div className="row">
-                <div className="col-md-10 mb-5" id="trackList">
+      <div className="col-12 col-md-9 offset-md-3 mainPage">
+        <div className="row">
+          <div className="col-md-3 pt-5 text-center" id="img-container">{this.albumArt(this.state.albums, this.state.artist)}</div>
+          <div className="col-md-8 p-5">
+            <div className="row">
+              <div className="col-md-10 mb-5" id="trackList">
                 {this.song(this.state.tracks)}
-                  </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
     )
   }
 }
 
-export default Album
+export default connect(mapStateToProps, mapDispatchToProps)(Album)
