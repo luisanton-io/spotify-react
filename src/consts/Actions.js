54aab0f62af232e4963decf8403c5b1bd670ede4
Luis Antonio Canettoli <hello@luisanton.io>
2020-07-31 16:36:38 +0200

    Update Actions.js

--- src/consts/Actions.js
+++ src/consts/Actions.js
@@ -7,6 +7,9 @@ const Actions = {
     toggleLike: {
         like: "LIKE_SONG",
         unlike: "UNLIKE_SONG"
+    },
+    login: {
+        setUsername: "SET_USERNAME"
     }
 }
 export default Actions
