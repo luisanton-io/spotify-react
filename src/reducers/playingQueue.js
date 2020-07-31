6cdb1864ed74cea76a8a753d9aa747b9d84b6d66
Luis Antonio Canettoli <hello@luisanton.io>
2020-07-31 16:40:46 +0200

    Update playingQueue.js

--- src/reducers/playingQueue.js
+++ src/reducers/playingQueue.js
@@ -7,16 +7,6 @@ export default function (state = {}, action) {
                 nowPlaying: action.payload.track,
                 album: action.payload.album
             }
-        // case Actions.playingQueue.add: 
-        //     return {
-        //         ...state,
-        //         list: state.list.concat(action.payload)
-        //     }
-        // case Actions.playingQueue.remove:
-        //     return {
-        //         ...state,
-        //         list: state.list.filter(song => song !== action.payload)
-        //     }
         default: return state;
     }
 }
